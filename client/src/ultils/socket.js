9c9e63dbd6594d44cdc3d9e42f933d916faa0f5b
ITITIU21171 <118947331+ITITIU21171@users.noreply.github.com>
2025-08-22 14:34:57 +0700

    add socket console
    add socket console

--- client/src/ultils/socket.js
+++ client/src/ultils/socket.js
@@ -6,6 +6,14 @@ export const socket = io(SOCKET_URL, {
   autoConnect: true,
 });
 
+socket.on('connect', () => {
+  console.log('Socket connected:', socket.id);
+  window.socket = socket; 
+});
+socket.on('disconnect', () => {
+  console.log('Socket disconnected');
+});
+
 export function inviteMemberSocket(inviteData) {
   socket.emit('invite-member', inviteData);
 }
